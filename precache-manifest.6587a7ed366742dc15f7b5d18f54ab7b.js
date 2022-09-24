747b18e22699565b60cf080f854af8c3803a33a4
Bharath <bharath.pulindram@gmail.com>
2022-09-24 11:28:04 -0500

    Updates

---
+++ precache-manifest.6587a7ed366742dc15f7b5d18f54ab7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054b705b21005a21e97b69f4a3d09fe6",
+    "url": "/Tinder/index.html"
+  },
+  {
+    "revision": "a36da9c50c33cd1c2a8f",
+    "url": "/Tinder/static/css/main.cd0ff7ef.chunk.css"
+  },
+  {
+    "revision": "477b386a8ebb3c91c1d6",
+    "url": "/Tinder/static/js/2.65c50e2c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Tinder/static/js/2.65c50e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36da9c50c33cd1c2a8f",
+    "url": "/Tinder/static/js/main.ad31ab34.chunk.js"
+  },
+  {
+    "revision": "f4ca9a75dbf8ca7258c3",
+    "url": "/Tinder/static/js/runtime-main.d0c76f57.js"
+  }
+]);
